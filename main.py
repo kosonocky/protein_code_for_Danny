747757c4170dd091e78c52e2cae6b0c108b75468
kosonocky <clayton.kosonocky@gmail.com>
2021-11-18 09:48:11 -0800

    test

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from data_analysis import count_amino_acids
 
 def main():
     working_directory = os.path.dirname(os.path.abspath(__file__))
-    txtfile_name = 'pdbids.txt'
+    txtfile_name = 'pdbids.txt' #
     pdbids_list = get_pdbids_from_txt(directory = working_directory, filename = txtfile_name)
     download_cif_files_from_rcsb(directory = working_directory, name_list= pdbids_list)
 
